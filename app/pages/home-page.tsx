19bbde16d6bbfa01f5748f2f783570945a92f8dd
keeekeeey <snowballintexas@gmail.com>
2026-02-02 13:37:05 -0600

    refactor: reorganize pages to be modular

--- app/pages/home-page.tsx
+++ app/pages/home-page.tsx
@@ -1,19 +1,12 @@
-"use client"
 
+import DraggableWindow from "../componets/DraggableWindow";
 import { useState, useRef } from "react";
-import WindowManager from "./componets/WindowManager";
 
 export default function HomePage() {
+    const [isVisible, setIsVisible] = useState(false);
 
 return (
     <div className="w-full h-full">
-      <div className="basis-1/4 border rounded p-4 bg-foreground-02 grid grid-cols-2 gap-4"> 
-        <div className="border rounded-2xl bg-background min-h-5 px-5 flex items-center"></div>
-        <button 
-        className="bg-foreground-03 rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-accent">
-          X
-        </button>
-      </div>
           <div className="basis-2/4 border rounded p-2 bg-foreground-01">
               <div className="p-4 border min-h-120">
                 page
@@ -21,7 +14,6 @@ return (
                     <div className="p-1">
                       <p className="bg-foreground-02 p-1">If you're looking for a problem solver, look no further!</p>
                       words
-                      <WindowManager></WindowManager>
                       </div>
                     <div>
                       <img
@@ -31,7 +23,7 @@ return (
                   </div>
                   <div className="min-h-10 border bg-[url(https://i.pinimg.com/1200x/04/c4/99/04c499e22cdf26db8a48f17372b04bb8.jpg)]">
                   </div>
-                    <div className="flex items-center gap-2 border border-amber-500" >
+                    <div className="flex items-center gap-2" >
                       <button 
                         className="bg-foreground-02 rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-accent">
                           <img className="max-h-10 max-w-10" src="https://cdn-icons-png.flaticon.com/256/25/25231.png"></img>
@@ -40,7 +32,11 @@ return (
                         className="bg-foreground-02 rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-accent">
                           <img className="mx-h-10 max-w-10" src="https://cdn-icons-png.flaticon.com/128/3536/3536569.png"></img>
                       </button>
-                      
+                      <button 
+                        onClick={() => (setIsVisible(!isVisible))}
+                        className="bg-foreground-03 rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-accent">
+                          Resume
+                      </button>
                   </div>
               </div>
           </div>
