f48b8cfad00e319cd8cdb3408bfcc5841e16e763
keeekeeey <snowballintexas@gmail.com>
2026-02-03 11:50:33 -0600

    feat: timer widget

---
+++ app/componets/Timer.tsx
@@ -0,0 +1,20 @@
+import { useState , useEffect } from "react"
+
+export default function TimerWidget (){
+
+    const [currTime, setCurrTime] = useState(new Date ().toLocaleDateString())
+
+    useEffect(() => {
+        const interval = setInterval(()=> {
+            setTimeout( new Date().toLocaleDateString());
+        }, 1000)
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return(
+        <div>
+            {currTime}
+        </div>
+    )
+}
