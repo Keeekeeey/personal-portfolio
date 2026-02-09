54b1ad04c5c3ae74aec34be32fe9d19f7d1658d2
keeekeeey <snowballintexas@gmail.com>
2026-02-09 12:44:34 -0600

    feat: enable github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath:'/keeekeeey.github.io',
+  images:{
+    unoptimized: true,
+  },
 };
+
+module.exports = nextConfig
 
 export default nextConfig;
