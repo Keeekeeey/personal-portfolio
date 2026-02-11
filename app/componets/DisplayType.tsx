6a045c00e831243af0595ceb9865aed0cf0413aa
keeekeeey <snowballintexas@gmail.com>
2026-02-11 15:30:22 -0600

    fix: files renamed to accurately reflect functions

--- app/componets/DisplayType.tsx
+++ app/componets/DisplayType.tsx
@@ -1,7 +1,7 @@
 'use client'
 import useIsMobile from "../api/useIsMobile";
 
-import DesktopLandingPage from "../pages/desktop-screen";
+import DesktopLandingPage from "../pages/desktop-landing-page";
 import MobileLandingPage from "../pages/mobile-landing-page";
 
 export default function DisplayType (){
